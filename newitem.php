e38e05612c9da3ad05b3117e933b550db26cbb1e
calvintts <calvinteytaisoon@gmail.com>
2017-12-05 13:32:48 -0800

    changed server.php and added newitem.php

---
+++ newitem.php
@@ -0,0 +1,28 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8" />
+<title>New Item</title>
+</head>
+<body>
+<script>
+  function newphone(){
+    let x= document.getElementById("form_input");
+    let content = "<div id = \"box\"><form method=\"post\" action=\"newitem.php\"><div class=\"input-group\"><label>Camera</label><input type=\"cam\" name=\"cam\" ></div><div class=\"input-group\"><label>Brand</label> <input type=\"brand\" name=\"pbrand\"></div><div class=\"input-group\"><label>Comment</label> <input type=\"cmt\" name=\"pcmt\"></div><div class=\"input-group\"><button type=\"submit\" class=\"btn\" name=\"savephone\">Save Phone</button></div></form></div>";
+    x.innerHTML=content;}
+  function newcar(){
+    let x=document.getElementById("form_input");
+    let content = "<div id = \"box\"><form method=\"post\" action=\"newitem.php\"><div class=\"input-group\"><label>Horsepower</label><input type=\"horsepower\" name=\"hp\" ></div><div class=\"input-group\"><label>Brand</label> <input type=\"brand\" name=\"cbrand\"></div><div class=\"input-group\"><label>Year</label> <input type=\"year\" name=\"year\"></div><div class=\"input-group\"><label>Comment</label> <input type=\"cmt\" name=\"ccmt\"></div><div class=\"input-group\"><button type=\"submit\" class=\"btn\" name=\"savecar\">Save Car</button></div></form></div>";
+    x.innerHTML=content;
+  }
+</script>
+<!-- navigation button -->
+<section>
+<button onclick="newcar()">Add Car</button>
+<button onclick="newphone()">Add Phone</button>
+<div id="form_input"></div>
+</section>
+
+</body>
+</html>
--- server.php
+++ server.php
@@ -4,12 +4,41 @@ session_start();
 // variable declaration
 $username = "";
 $email    = "";
-$errors = array(); 
+$errors = array();
 $_SESSION['success'] = "";
 
 // connect to database
 $db = mysqli_connect('localhost', 'root', '', 'registration');
+/////my code starts here
+if(isset($_POST['savephone'])){
+$pcomment = mysqli_real_escape_string($db, $_POST['pcmt']);
+//$finalcomment=$pcomment."by".$username;
+$camera = mysqli_real_escape_string($db, $_POST['cam']);
+$pbrand = mysqli_real_escape_string($db, $_POST['pbrand']);
 
+$qry = "INSERT INTO phone (camera, brand, comment)
+VALUES ('$camera', '$pbrand', '$pcomment')";
+
+mysqli_query($db,$qry);
+}
+
+//$db = mysqli_connect('localhost', 'root', '', 'registration');
+/////my code starts here
+if(isset($_POST['savecar'])){
+$ccomment = mysqli_real_escape_string($db, $_POST['ccmt']);
+$hp = mysqli_real_escape_string($db, $_POST['hp']);
+$cbrand = mysqli_real_escape_string($db, $_POST['cbrand']);
+$year = mysqli_real_escape_string($db, $_POST['year']);
+
+$qry = "INSERT INTO car (brand, year,horsepower, comment)
+VALUES ('$cbrand', '$year', '$hp','$ccomment')";
+
+mysqli_query($db,$qry);
+}
+
+
+
+/////end here
 // REGISTER USER
 if (isset($_POST['reg_user'])) {
   // receive all input values from the form
@@ -29,7 +58,7 @@ if (isset($_POST['reg_user'])) {
   // register user if there are no errors in the form
   if (count($errors) == 0) {
   	$password = md5($password_1);//encrypt the password before saving in the database
-  	$query = "INSERT INTO users (username, email, password) 
+  	$query = "INSERT INTO users (username, email, password)
   			  VALUES('$username', '$email', '$password')";
   	mysqli_query($db, $query);
   	$_SESSION['username'] = $username;

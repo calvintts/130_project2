2834967945543078b3d00f0cd96e949ee1c96562
calvintts <calvinteytaisoon@gmail.com>
2017-12-05 20:25:56 -0800

    Add files via upload

--- server.php
+++ server.php
@@ -1,22 +1,112 @@
 <?php
 session_start();
-
 // variable declaration
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+
 $username = "";
 $email    = "";
-$errors = array(); 
+$errors = array();
 $_SESSION['success'] = "";
 
+//Database creation and connection
+$sql = "CREATE DATABASE registration";
+
+$conn = new mysqli($host,$user,$pass);
+// if($conn->query($sql))
+	// echo "connected";
+	// else
+	// {
+		// echo "not connected";
+	// }
+if($conn->connect_error){
+	die("Connection failed".$conn->connect_error);}
+
+$conn->close();
+
+
+
 // connect to database
-$db = mysqli_connect('localhost', 'root', '', 'registration');
+$conn = mysqli_connect($host, $user, $pass, 'registration');
+if($conn->connect_error)
+	echo "not connected";
+
+//DROP TABLE IF EXISTS `users`;
+ $use="CREATE TABLE `users`(
+	id			INTEGER(4)		NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	username	VARCHAR(100)	NOT NULL ,
+	password	VARCHAR(100)	NOT NULL ,
+	email		VARCHAR(100)	NOT NULL )";
+	$conn->query($use);
+
+//DROP TABLE IF EXISTS `items`;
+$ite = "CREATE TABLE `items`(
+	iid			INTEGER(3)		NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	color		VARCHAR(100)	NOT NULL ,
+	price		INTEGER(100)	NOT NULL )";
+	$conn->query($ite);
+
+//DROP TABLE IF EXISTS `phone`;
+$pho = "CREATE TABLE `phone`(
+	pid			INTEGER(4)		PRIMARY KEY REFERENCES items (iid),
+	camera		VARCHAR(100)	NOT NULL ,
+	brand		VARCHAR(100)	NOT NULL ,
+	comment		TEXT(100)		NOT NULL ,
+	name		VARCHAR(100)	NOT NULL )";
+	$conn->query($pho);
+
+
+//DROP TABLE IF EXISTS `car`;
+$car ="CREATE TABLE `car`(
+	cid			INTEGER(4)		PRIMARY KEY REFERENCES items (iid),
+	brand		VARCHAR(100)	NOT NULL ,
+	year		INTEGER(100)	NOT NULL ,
+	horsepower	INTEGER(100)	NOT NULL ,
+	comment		TEXT(100)		NOT NULL,
+	name		VARCHAR(100)	NOT NULL 	)";
+	$conn->query($car);
+
+if(isset($_POST['savephone'])){
+$pcomment = mysqli_real_escape_string($conn, $_POST['pcmt']);
+//$finalcomment=$pcomment."by".$username;
+$color = mysqli_real_escape_string($conn, $_POST['clr']);
+$price = mysqli_real_escape_string($conn, $_POST['price']);
+$camera = mysqli_real_escape_string($conn, $_POST['cam']);
+$pbrand = mysqli_real_escape_string($conn, $_POST['pbrand']);
+$u=$_SESSION['username'];
+$qry = "INSERT INTO phone(camera, brand, comment,name)
+VALUES ('$camera', '$pbrand', '$pcomment','$u')";
+$qr = "INSERT INTO items(color, price)
+VALUES ('$color', '$price')";
+mysqli_query($conn,$qry);
+mysqli_query($conn,$qr);
+}
+//$ = mysqli_connect('localhost', 'root', '', 'registration');
+/////my code starts here
+if(isset($_POST['savecar'])){
+$color = mysqli_real_escape_string($conn, $_POST['clr']);
+$price = mysqli_real_escape_string($conn, $_POST['price']);
+$ccomment = mysqli_real_escape_string($conn, $_POST['ccmt']);
+$hp = mysqli_real_escape_string($conn, $_POST['hp']);
+$cbrand = mysqli_real_escape_string($conn, $_POST['cbrand']);
+$year = mysqli_real_escape_string($conn, $_POST['year']);
+$u=$_SESSION['username'];
+$qry = "INSERT INTO car(brand, year,horsepower, comment,name)
+VALUES ($cbrand', '$year', '$hp','$ccomment','$u')";
+$qr = "INSERT INTO items(color, price)
+VALUES ('$color', '$price')";
+mysqli_query($conn,$qry);
+mysqli_query($conn,$qr);
+}
 
 // REGISTER USER
 if (isset($_POST['reg_user'])) {
   // receive all input values from the form
-  $username = mysqli_real_escape_string($db, $_POST['username']);
-  $email = mysqli_real_escape_string($db, $_POST['email']);
-  $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
-  $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+  $username = mysqli_real_escape_string($conn, $_POST['username']);
+  $email = mysqli_real_escape_string($conn, $_POST['email']);
+  $password_1 = mysqli_real_escape_string($conn, $_POST['password_1']);
+  $password_2 = mysqli_real_escape_string($conn, $_POST['password_2']);
 
   // form validation: ensure that the form is correctly filled
   if (empty($username)) { array_push($errors, "Username is required"); }
@@ -29,9 +119,9 @@ if (isset($_POST['reg_user'])) {
   // register user if there are no errors in the form
   if (count($errors) == 0) {
   	$password = md5($password_1);//encrypt the password before saving in the database
-  	$query = "INSERT INTO users (username, email, password) 
+  	$query = "INSERT INTO users (username, email, password)
   			  VALUES('$username', '$email', '$password')";
-  	mysqli_query($db, $query);
+  	mysqli_query($conn, $query);
   	$_SESSION['username'] = $username;
   	$_SESSION['success'] = "You are now logged in";
   	header('location: index.php');
@@ -40,8 +130,8 @@ if (isset($_POST['reg_user'])) {
 }
 // LOGIN USER
 if (isset($_POST['login_user'])) {
-  $username = mysqli_real_escape_string($db, $_POST['username']);
-  $password = mysqli_real_escape_string($db, $_POST['password']);
+  $username = mysqli_real_escape_string($conn, $_POST['username']);
+  $password = mysqli_real_escape_string($conn, $_POST['password']);
 
   if (empty($username)) {
   	array_push($errors, "Username is required");
@@ -53,7 +143,7 @@ if (isset($_POST['login_user'])) {
   if (count($errors) == 0) {
   	$password = md5($password);
   	$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
-  	$results = mysqli_query($db, $query);
+  	$results = mysqli_query($conn, $query);
   	if (mysqli_num_rows($results) == 1) {
   	  $_SESSION['username'] = $username;
   	  $_SESSION['success'] = "You are now logged in";
@@ -63,4 +153,13 @@ if (isset($_POST['login_user'])) {
   	}
   }
 }
+ //upload image to database
+// $imagename = $_FILES["myimage"]["name"];
+
+// //get the content of the image and then add slashes to it
+// $imagetmp = addslashes (file_get_contents($_FILES['myimage']['tmp_name']));
+
+// //insert the image name and image content in image_table
+// $insert_image = "INSERT INTO items VALUES ('$imagetmp','$imagename')";
+// mysql_query($insert_image);
 ?>

5530be741cf6cc5f6aa2e563c5884607b313027b
calvintts <calvinteytaisoon@gmail.com>
2017-12-05 17:30:20 -0800

    Add files via upload

--- newitem.php
+++ newitem.php
@@ -9,11 +9,11 @@
 <script>
   function newphone(){
     let x= document.getElementById("form_input");
-    let content = "<div id = \"box\"><form method=\"post\" action=\"newitem.php\"><div class=\"input-group\"><label>Camera</label><input type=\"cam\" name=\"cam\" ></div><div class=\"input-group\"><label>Brand</label> <input type=\"brand\" name=\"pbrand\"></div><div class=\"input-group\"><label>Comment</label> <input type=\"cmt\" name=\"pcmt\"></div><div class=\"input-group\"><button type=\"submit\" class=\"btn\" name=\"savephone\">Save Phone</button></div></form></div>";
+    let content = "<div id = \"box\"><form method=\"post\" action=\"newitem.php\"><div class=\"input-group\"><label>Color</label><input type=\"clr\" name=\"clr\" ></div><div class=\"input-group\"><label>Price</label><input type=\"price\" name=\"price\" ></div><div class=\"input-group\"><label>Camera</label><input type=\"cam\" name=\"cam\" ></div><div class=\"input-group\"><label>Brand</label> <input type=\"brand\" name=\"pbrand\"></div><div class=\"input-group\"><label>Comment</label> <input type=\"cmt\" name=\"pcmt\"></div><div class=\"input-group\"><button type=\"submit\" class=\"btn\" name=\"savephone\">Save Phone</button></div></form></div>";
     x.innerHTML=content;}
   function newcar(){
     let x=document.getElementById("form_input");
-    let content = "<div id = \"box\"><form method=\"post\" action=\"newitem.php\"><div class=\"input-group\"><label>Horsepower</label><input type=\"horsepower\" name=\"hp\" ></div><div class=\"input-group\"><label>Brand</label> <input type=\"brand\" name=\"cbrand\"></div><div class=\"input-group\"><label>Year</label> <input type=\"year\" name=\"year\"></div><div class=\"input-group\"><label>Comment</label> <input type=\"cmt\" name=\"ccmt\"></div><div class=\"input-group\"><button type=\"submit\" class=\"btn\" name=\"savecar\">Save Car</button></div></form></div>";
+    let content = "<div id = \"box\"><form method=\"post\" action=\"newitem.php\"><div class=\"input-group\"><label>Color</label><input type=\"clr\" name=\"clr\" ></div><div class=\"input-group\"><label>Price</label><input type=\"price\" name=\"price\" ></div><div class=\"input-group\"><label>Horsepower</label><input type=\"horsepower\" name=\"hp\" ></div><div class=\"input-group\"><label>Brand</label> <input type=\"brand\" name=\"cbrand\"></div><div class=\"input-group\"><label>Year</label> <input type=\"year\" name=\"year\"></div><div class=\"input-group\"><label>Comment</label> <input type=\"cmt\" name=\"ccmt\"></div><div class=\"input-group\"><button type=\"submit\" class=\"btn\" name=\"savecar\">Save Car</button></div></form></div>";
     x.innerHTML=content;
   }
 </script>
